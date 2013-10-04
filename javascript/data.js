e579402c7db45accd4e02e51b7e66d8e2b971c5e
Adrien Lo <adrien.lo@gmail.com>
2013-10-04 15:33:50 -0700

    initial framework checkin
    broke something in project details. so had to revert before checking in.

---
+++ javascript/data.js
@@ -0,0 +1,96 @@
+var Data = {
+	Projects: {
+		categories: [
+			{
+				id: 1,
+				name: "residential"
+			},
+			{
+				id: 2,
+				name: "commercial"
+			},
+			{
+				id: 3,
+				name: "interiors"
+			},
+			{
+				id: 4,
+				name: "other"
+			}
+		],
+		projects: [
+			{
+				name: "Food",
+				description: "",
+				caption: "",
+				subcaption: "",
+				thumbnail: "img/proj_01.jpg",
+				categories: [],
+				assets: []
+			},
+			{
+				name: "Food",
+				description: "",
+				caption: "",
+				subcaption: "",
+				thumbnail: "img/proj_01.jpg",
+				categories: [],
+				assets: []
+			},
+			{
+				name: "Food",
+				description: "",
+				caption: "",
+				subcaption: "",
+				thumbnail: "img/proj_01.jpg",
+				categories: [],
+				assets: []
+			},
+			{
+				name: "Food",
+				description: "",
+				caption: "",
+				subcaption: "",
+				thumbnail: "img/proj_01.jpg",
+				categories: [],
+				assets: []
+			},
+			{
+				name: "Food",
+				description: "",
+				caption: "",
+				subcaption: "",
+				thumbnail: "img/proj_01.jpg",
+				categories: [],
+				assets: []
+			},
+			{
+				name: "Food",
+				description: "",
+				caption: "",
+				subcaption: "",
+				thumbnail: "img/proj_01.jpg",
+				categories: [],
+				assets: []
+			},
+			{
+				name: "Food",
+				description: "",
+				caption: "",
+				subcaption: "",
+				thumbnail: "img/proj_01.jpg",
+				categories: [],
+				assets: []
+			},
+			{
+				name: "Food",
+				description: "",
+				caption: "",
+				subcaption: "",
+				thumbnail: "img/proj_01.jpg",
+				categories: [],
+				assets: []
+			}
+		]
+	}
+};
