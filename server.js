402a5a8e32273dc09b8d25ebf42e05bc41053e8f
Adrien Lo <adrien.lo@gmail.com>
2014-09-03 21:34:01 -0700

    updates for retina

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var ecstatic = require('ecstatic');
 
 var app = express();
 app.use(ecstatic({ root: __dirname }));
-http.createServer(app).listen(8081);
+http.createServer(app).listen(8088);
 
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8088/');
